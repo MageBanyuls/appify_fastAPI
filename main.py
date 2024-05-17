b2eebc1020c233e7d26774d9a8fd7a3aaf55d10c
Magellan <magebanyuls@gmail.com>
2024-05-17 12:52:00 -0300

    Documentacion

--- main.py
+++ main.py
@@ -1,19 +1,77 @@
 # main.py
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import List
 from utils import peticion_get, peticion_post
 from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
 
 
+class IdDoc(BaseModel):
+    TipoDTE: int
+    FchEmis: str
+    Folio: str
+
+class Emisor(BaseModel):
+    RUTEmisor: str
+
+class Receptor(BaseModel):
+    RUTRecep: str
+    RznSocRecep: str
+    GiroRecep: str
+    Contacto: str
+    CorreoRecep: str
+    DirRecep: str
+    CmnaRecep: str
+
+class DetalleItem(BaseModel):
+    NmbItem: str
+    QtyItem: int
+    PrcItem: int
+
+class ReferenciaItem(BaseModel):
+    TpoDocRef: str
+    RazonRef: str
+    FolioRef: str
+
+class Encabezado(BaseModel):
+    IdDoc: IdDoc
+    Emisor: Emisor
+    Receptor: Receptor
+
+class DteTemporalData(BaseModel):
+    Encabezado: Encabezado
+    Detalle: List[DetalleItem]
+    Referencia:List[ReferenciaItem]
+
+class DteRealData(BaseModel):
+    codigo: str
+    dte: int
+    emisor: int
+    receptor: int
+
+# Define la función para el endpoint
 @app.post("/dte-temporal", tags=["Emisión de Documentos"])
-async def dte_temporal(request: Request):
+async def dte_temporal(request: Request, dte_data: DteTemporalData):
+    """
+    Emite un documento temporal.
+
+    Parámetros:
+    - dte_data: Datos del documento temporal a emitir.
+    """
     datos = await request.json()
     url = "https://libredte.cl/api/dte/documentos/emitir?normalizar=1&formato=json&links=0&email=0"
     return peticion_post(datos, url)
 
 @app.post("/dte-real", tags=["Emisión de Documentos"])
-async def dte_real(request: Request):
+async def dte_real(request: Request, dte_data :DteRealData):
+    """
+    Emite un documento real.
+
+    Parámetros:
+    - dte_data: Datos del documento real a emitir.
+    """
     datos = await request.json()
     url = "https://libredte.cl/api/dte/documentos/generar?getXML=0&links=0&email=0&retry=1&gzip=0"
     return peticion_post(datos, url)
