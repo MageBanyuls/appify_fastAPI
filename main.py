2b820ccc3553ca35b1858683b9220d8ce098e8de
Magellan <magebanyuls@gmail.com>
2024-05-20 09:35:41 -0300

    Comentado para describir que hace cada funcion y ruta

--- main.py
+++ main.py
@@ -78,40 +78,77 @@ async def dte_real(request: Request, dte_data :DteRealData):
 
 @app.get("/dte_tmps/info", tags=["Documentos Temporales"])
 async def info_tmps(codigo: str, dte: str, emisor: str, receptor: str):
+    """
+    Obtiene la informacion de un DTE temporal.
+
+    """
     url = f"https://libredte.cl/api/dte/dte_tmps/info/{receptor}/{dte}/{codigo}/{emisor}?getDetalle=0&getDatosDte=0&getEmailEnviados=0&getLinks=0&getReceptor=0&getSucursal=0&getUsuario=0"
     return peticion_get(url)
 
 @app.get("/dte_tmps/pdf", tags=["Documentos Temporales"])
 async def pdf_tmps(codigo: str, dte: str, emisor: str, receptor: str):
+    """
+    
+    Obtiene el PDF de un DTE temporal.
+
+    """
     url = f"https://libredte.cl/api/dte/dte_tmps/pdf/{receptor}/{dte}/{codigo}/{emisor}?cotizacion=0&formato=general&papelContinuo=0&compress=0"
     return peticion_get(url)
 
 @app.get("/dte_tmps/xml", tags=["Documentos Temporales"])
 async def xml_tmps(codigo: str, dte: str, emisor: str, receptor: str):
+    """
+    Obtiene el XML de un DTE temporal.
+
+    """
     url = f"https://libredte.cl/api/dte/dte_tmps/xml/{receptor}/{dte}/{codigo}/{emisor}"
     return peticion_get(url)
 
 @app.get("/dte_tmps/eliminar", tags=["Documentos Temporales"])
 async def eliminar_tmps(codigo: str, dte: str, emisor: str, receptor: str):
+    """
+
+    Elimina los datos de un DTE temporal
+    
+    """
     url = f"https://libredte.cl/api/dte/dte_tmps/eliminar/{receptor}/{dte}/{codigo}/{emisor}"
     return peticion_get(url)
 
 @app.get("/dte_emitidos/info", tags=["Documentos Emitidos"])
 async def info_emitidos(dte: str, emisor: str, folio: str):
+    """
+    Obtiene la informacion de un DTE real.
+
+    """
     url = f"https://libredte.cl/api/dte/dte_emitidos/info/{dte}/{folio}/{emisor}"
     return peticion_get(url)
 
 @app.get("/dte_emitidos/pdf", tags=["Documentos Emitidos"])
 async def pdf_emitidos(dte: str, emisor: str, folio: str):
+    """
+    
+    Obtiene el PDF de un DTE real.
+
+    """
     url = f"https://libredte.cl/api/dte/dte_emitidos/pdf/{dte}/{folio}/{emisor}"
     return peticion_get(url)
 
 @app.get("/dte_emitidos/xml", tags=["Documentos Emitidos"])
 async def xml_emitidos(dte: str, emisor: str, folio: str):
+    """
+
+    Obtiene el XML de un DTE temporal.
+
+    """
     url = f"https://libredte.cl/api/dte/dte_emitidos/xml/{dte}/{folio}/{emisor}"
     return peticion_get(url)
 
 @app.get("/dte_emitidos/actualizarestado", tags=["Documentos Emitidos"])
 async def actualizar_estado(tipo: str, folio: str, emisor: str):
+    """
+    
+    Actualiza el estado de un DTE real.
+
+    """
     url = f"https://libredte.cl/api/dte/dte_emitidos/actualizar_estado/{tipo}/{folio}/{emisor}?usarWebservice=1"
     return peticion_get(url)
